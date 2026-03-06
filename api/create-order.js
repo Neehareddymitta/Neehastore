5aec6c204d506e0fd840ba5d0204c18cf4cf28c5
Neehareddymitta <77287341+Neehareddymitta@users.noreply.github.com>
2026-03-06 20:17:23 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -1,9 +1,11 @@
 export default async function handler(req, res) {
+
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
 
   try {
+
     const { orderAmount, customerName, customerPhone, customerEmail } = req.body;
 
     const response = await fetch("https://sandbox.cashfree.com/pg/orders", {
@@ -15,21 +17,28 @@ export default async function handler(req, res) {
         "x-api-version": "2022-09-01"
       },
       body: JSON.stringify({
-  order_id: "order_" + Date.now(),
-  order_amount: orderAmount,
-  order_currency: "INR",
-  customer_details: {
-    customer_id: "cust_" + Date.now(),
-    customer_name: customerName,
-    customer_email: customerEmail,
-    customer_phone: customerPhone
-  }
-})
+        order_id: "order_" + Date.now(),
+        order_amount: orderAmount,
+        order_currency: "INR",
+        customer_details: {
+          customer_id: "cust_" + Date.now(),
+          customer_name: customerName,
+          customer_email: customerEmail,
+          customer_phone: customerPhone
+        },
+        order_meta: {
+          return_url: "https://neehastore.vercel.app"
+        }
+      })
+    });
 
     const data = await response.json();
+
     return res.status(200).json(data);
 
   } catch (error) {
+    console.error("Cashfree error:", error);
     return res.status(500).json({ error: "Payment initiation failed" });
   }
+
 }
